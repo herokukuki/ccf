3161f7dd876e1d7c2a76a95644acda315296a5b0
Artur Stepovyi <astepovy@e3r4p1.unit.ua>
2017-09-21 14:21:38 +0300

    downloader path fixed

--- libs/downloader/index.js
+++ libs/downloader/index.js
@@ -6,7 +6,7 @@ var torrentStream = require('torrent-stream');
 var downloadMovie = (magnet) => {
     return new Promise(resolve => {
         var engine = torrentStream(magnet, {
-            path: 'public/videos/',
+            path: 'tmp/videos/',
         });
 
         var count = 0;
@@ -17,11 +17,13 @@ var downloadMovie = (magnet) => {
                 var format = file.name.split('.').pop();
                 filesNum = filesNum + 1;
                 console.log('filename:', file.name);
-                var stream = file.createReadStream();
+                var stream = file.createReadStream(file);
                 console.log('Torrent file download started');
                 if (format === 'mp4' || format === 'webm' || format === 'ogg' || format === 'mkv') {
                     console.log('matching movie format');
                     console.log('Save this name: ' + file.name);
+                    console.log('path is the following: ' + 'public/videos/' + file.name)
+	                resolve(fs.createReadStream('public/videos/' + file.name).pipe(fs.createWriteStream('public/videos/' + file.name)));
                 }
                 else {
                     console.log('non-supported video format or other type of file');
